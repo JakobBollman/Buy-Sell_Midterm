649e6171ae12c24264cc5f8e914980fa6a246550
Taylor Smith <taylor.l.smith07@gmail.com>
2022-10-22 15:04:33 -0700

    added seeds to listings and favourites

---
+++ db/seeds/03_comments.sql
@@ -0,0 +1,8 @@
+INSERT INTO comments (user_id, listing_id, content)
+VALUES
+(15, 10, 'Wow I''ll take three! What a great mayor we have.'),
+(14, 10, 'These seem a little expensive for just a yard sign..'),
+(8, 10, 'Are you ****ing kicking me!? THe mayor is a dang crook!!1!!'),
+(8, 11, 'Nobody buy this garbage'),
+(15, 11, )
+;
---
+++ db/seeds/04_favourites.sql
@@ -0,0 +1,7 @@
+INSERT INTO favourites (user_id, listing_id)
+VALUES
+(8, 2),
+(8, 4),
+(8, 7),
+(8, 1)
+;

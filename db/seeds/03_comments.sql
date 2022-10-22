260b91a69f0963d2b835a24585ef13f764973362
Taylor Smith <taylor.l.smith07@gmail.com>
2022-10-22 15:17:55 -0700

    Added seed comments

--- db/seeds/03_comments.sql
+++ db/seeds/03_comments.sql
@@ -4,5 +4,9 @@ VALUES
 (14, 10, 'These seem a little expensive for just a yard sign..'),
 (8, 10, 'Are you ****ing kicking me!? THe mayor is a dang crook!!1!!'),
 (8, 11, 'Nobody buy this garbage'),
-(15, 11, )
+(15, 11, 'Wow thanks Mayor, this totally cured me!'),
+(8, 11, 'Nice fake account Mayor, can you guys believe this?'),
+(10, 11, 'LOL'),
+(11, 11, 'Woooooooow'),
+(8, 5, 'How many of these could I get for tonight? No reason..')
 ;

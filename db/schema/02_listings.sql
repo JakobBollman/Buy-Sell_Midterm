3bfd7ed25cd58332c8e95b5ec8f4013e8f4f3f08
Taylor Smith <taylor.l.smith07@gmail.com>
2022-10-22 14:11:27 -0700

    added seeds for listings

--- db/schema/02_listings.sql
+++ db/schema/02_listings.sql
@@ -1,9 +1,12 @@
 DROP TABLE IF EXISTS listings CASCADE;
+DROP TYPE categories;
+CREATE TYPE categories AS ENUM ('Farming', 'Lighting', 'Misc', 'Food');
 CREATE TABLE listings (
   id SERIAL PRIMARY KEY NOT NULL,
   user_id INTEGER REFERENCES users(id),
   title VARCHAR(255) NOT NULL,
   description TEXT,
   price INTEGER NOT NULL,
+  category categories,
   status VARCHAR(10) DEFAULT 'active'
 );
--- db/seeds/02_listings.sql
+++ db/seeds/02_listings.sql
@@ -1,4 +1,15 @@
--- Widgets table seeds here (Example)
-INSERT INTO widgets (name, user_id) VALUES ('Sprockets', 1);
-INSERT INTO widgets (name, user_id) VALUES ('Chains', 2);
-INSERT INTO widgets (name, user_id) VALUES ('Bearings', 2);
+INSERT INTO listings (user_id, title, description, price, category, status)
+VALUES
+(12, 'New Pitchfork', 'Fresh from the blacksmith''s shop these pitchforks won''t leave you wanting. Move hay, pose with it in your yearly family photo, really show off your farming skills. \n Not intended for use as weapon.', 25, 'Farming', 'active'),
+(12, 'Rusty Pitchfork', 'For the farmer who wants a pitchfork with a little more personality. You don''t want to get poked by this bad boy, espiecially since tetanus shots don''t exist. Limited quantities available.', 15, 'Farming', 'active'),
+(9, 'Small Torch', 'Child sized torch, not too heavy. Made with the finest rags, sticks, and oil. Guaranteed burn time of 2 hours. Perfect to find your way through the village on those dark nights.', 10, 'Lighting', 'active'),
+(9, 'Medium Torch', 'Mid sized torch. Guranteed burn time of 3 hours, but still light enough to swing around if needed. Casts bright light in a15ft radius and dim light for another 15ft.', 15, 'Lighting', 'active'),
+(9, 'Large Torch', 'Deluxe torch. Burn time of up to 4 hours. Casts bright light in a 20ft radius, and dim light for another 20ft. Villager must be strong enough to carry it.', 25, 'Lighting', 'active'),
+(10, 'Ripe Tomatoe', 'Unsold tomatoes, extra ripe. Very aerodynamic, squishy, with a strong aroma. Also available in crate format.', 1, 'Food', 'active'),
+(10, 'Crate of Ripe Tomatoes', 'Best value for ripe tomatoes. If your village get-together needs to ''feed a crowd'', then this is the product for you. Crate contains up to 25 ripe tomatoes', 5, 'Food', 'active'),
+(10, 'Hoe', 'Standard gardening equipment, and looks great in a photo. One size only.', 15, 'Farming', 'active'),
+(9, 'Bale of Hay', 'Feeds up to 5 horses. Great for makeshift structures as well.', 2, 'Farming', 'active'),
+(7, 'Re-Elect the Mayor Sign', 'Elegant and stylish addition to any property. Looks great in your front yard or in front of your shop.', 10, 'Misc', 'active'),
+(7, 'Snake Oil', 'All purpose remedy. Gets rid of common ailments, makes your crops grow larger, and even fights off the plague.', 30, 'Misc', 'active')
+;
+

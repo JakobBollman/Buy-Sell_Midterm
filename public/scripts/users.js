f9b41f9066b8e789ed82d2d15c022060606491e7
Taylor Smith <taylor.l.smith07@gmail.com>
2022-10-24 12:10:17 -0700

    added functions for submitting new comments

--- public/scripts/users.js
+++ public/scripts/users.js
@@ -1,5 +1,7 @@
 // Client facing scripts here
+
 $(() => {
+
   $('#fetch-users').on('click', () => {
     $.ajax({
       method: 'GET',
@@ -14,4 +16,34 @@ $(() => {
       }
     });
   });
+
+  
+
+
+
 });
+
+//takes new comments and converts to safe text
+const safeText = function (str) {
+  let div = document.createElement("div");
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+};
+
+//takes safe text and converts to HTML element
+const createCommentElement = function (data) {
+  const commentTime = timeago.format(data.created_at);
+  const commentBody = safeText(data.content.text);
+  const result =
+  //HTML here is taken from Tweeter, placeholder until we have the real page
+  `<article><header><span class="topTweetLine"><span class="tweeter-name"><img src="/images/profile-hex.png"><p>${data.user.name}</p></span><p>${data.user.email}</p></span><p>${commentBody}</p></header><footer class="tweetFeet"><span><h5>${commentTime}</h5></span><span class="icons"><i class="fa-solid fa-flag"></i><i class="fa-solid fa-heart"></i><i class="fa-solid fa-retweet"></i></span></footer></article>`
+  return result;
+};
+
+//takes new HTML and adds to end of comment list
+const renderComments = function (tweets) {
+  for (let tweet of tweets) {
+    const $tweet = createTweetElement(tweet);
+    $('.tweets').append($tweet);
+  }
+};

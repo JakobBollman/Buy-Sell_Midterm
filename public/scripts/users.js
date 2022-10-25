3e508fd2ce3944aa044725bff807f3fe633d1e22
Taylor Smith <taylor.l.smith07@gmail.com>
2022-10-24 18:16:04 -0700

    create comments query file and comment fetching functions

--- public/scripts/users.js
+++ public/scripts/users.js
@@ -28,27 +28,4 @@ $(() => {
 
 });
 
-//takes new comments and converts to safe text
-const safeText = function (str) {
-  let div = document.createElement("div");
-  div.appendChild(document.createTextNode(str));
-  return div.innerHTML;
-};
 
-//takes safe text and converts to HTML element
-const createCommentElement = function (data) {
-  const commentTime = timeago.format(data.created_at);
-  const commentBody = safeText(data.content.text);
-  const result =
-  //HTML here is taken from my Tweeter, placeholder until we have the real page
-  `<article><header><span class="topTweetLine"><span class="tweeter-name"><img src="/images/profile-hex.png"><p>${data.user.name}</p></span><p>${data.user.email}</p></span><p>${commentBody}</p></header><footer class="tweetFeet"><span><h5>${commentTime}</h5></span><span class="icons"><i class="fa-solid fa-flag"></i><i class="fa-solid fa-heart"></i><i class="fa-solid fa-retweet"></i></span></footer></article>`
-  return result;
-};
-
-//takes new HTML and adds to end of comment list
-const renderComments = function (comments) {
-  for (let comment of comments) {
-    const $comment = createCommentElement(comment);
-    $('.comments').append($comment);
-  }
-};

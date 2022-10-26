4ddc0772c32c3f2c0ba61cbbaa8ccce6e233901d
Kei Kaloustian <kei.kaloustian@gmail.com>
2022-10-26 16:44:23 -0700

    Clean up app.js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,21 +1,6 @@
 $(document).ready(() => {
 
-  // EXAMPLE LISTENER TO BE DELETED------------------------------
-  // $('#fetch-users').on('click', () => {
-  //   $.ajax({
-  //     method: 'GET',
-  //     url: '/api/users'
-  //   })
-  //   .done((response) => {
-  //     const $usersList = $('#users');
-  //     $usersList.empty();
-
-  //     for(const user of response.users) {
-  //       $(`<li class="user">`).text(user.name).appendTo($usersList);
-  //     }
-  //   });
-  // });
-
+  
 
   //placeholder, my-listings will be whatever we call that link
 

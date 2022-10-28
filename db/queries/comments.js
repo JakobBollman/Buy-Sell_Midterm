6fe1017bd21a9c344679b3f960f72d75465e4ceb
Kei Kaloustian <kei.kaloustian@gmail.com>
2022-10-27 19:44:18 -0700

    Refactor all listings routes

--- db/queries/comments.js
+++ db/queries/comments.js
@@ -24,10 +24,11 @@ const createNewComment = (listingID, userID, comment) => {
   return db
   .query(`
   INSERT INTO comments (user_id, listing_id, content)
-  VALUES ($1, $2, $3)`,
+  VALUES ($1, $2, $3) RETURNING *`,
   [userID, listingID, comment])
 
   .then(data => {
+    return data.rows;
   })
 
   .catch(err => {
--- routes/listings.js
+++ routes/listings.js
@@ -9,22 +9,21 @@ const favouritesQueries = require('../db/queries/favourites');
 const usersQueries = require('../db/queries/users');
 const commentsQueries = require('../db/queries/comments');
 
-
+// GET /listings/my_listings
 router.get('/my_listings', (req, res) => {
+
   const templateVars = {}
   const userID = req.session.user_id;
+  templateVars.user = userID;
 
   listingsQueries.getMyListings(userID)
-
   .then((listingsData) => {
-    favouritesQueries.getFavouriteListings(userID)
-    .then((favListingsData) => {
-      templateVars.myListings = listingsData
-      templateVars.favourites = favListingsData
-      templateVars.user = userID
-      res.render('my_listings',templateVars);
-    })
-    .catch(errorMessage => res.send(errorMessage));
+    templateVars.myListings = listingsData;
+    return favouritesQueries.getFavouriteListings(userID);
+  })
+  .then((favListingsData) => {
+    templateVars.favourites = favListingsData;
+    res.render('my_listings',templateVars);
   })
   .catch(errorMessage => res.send(errorMessage));
 });
@@ -32,8 +31,10 @@ router.get('/my_listings', (req, res) => {
 
 // GET /listings/new
 router.get('/new', (req, res) => {
+
   const userID = req.session.user_id;
-  templateVars = {user : userID}
+  templateVars = { user : userID };
+
   res.render('new_listing', templateVars);
 });
 
@@ -42,163 +43,147 @@ router.get('/new', (req, res) => {
 router.get('/favourites', (req, res) => {
 
   const templateVars = {};
-
-  // Capture user id from cookie session
   const userID = req.session.user_id;
+  templateVars.user = userID;
 
-  // Query for all users
   usersQueries.getAllUsers()
   .then(usersData => {
-    favouritesQueries.getFavouriteListings(userID)
-    .then((favListingsData) => {
-      templateVars.users = usersData;
-      templateVars.favourites = favListingsData;
-      templateVars.user = userID;
-      res.render('favourites', templateVars);
-    })
-    .catch((errorMessage) => res.send(errorMessage));
+    templateVars.users = usersData;
+    return favouritesQueries.getFavouriteListings(userID);
   })
-  .catch((errorMessage) => res.send(errorMessage));
+  .then((favListingsData) => {
+    templateVars.favourites = favListingsData;
+    res.render('favourites', templateVars);
+  })
+  .catch(errorMessage => res.send(errorMessage));
 });
 
 
 // GET /listings/:id
 router.get('/:id', (req, res) => {
-  // Capture listing id
+
   const listingID = req.params.id;
   const userID = req.session.user_id;
+
   let temp = {};
+  temp.user = userID;
 
   usersQueries.getAllUsers()
   .then((UsersData) => {
-
-    temp.user = userID;
-
-    commentsQueries.getCommentsById(listingID)
-    .then((commentData) => {
-      listingsQueries.getListing(listingID)
-      .then((listingData) => {
-        temp.users = UsersData;
-        temp.comments = commentData;
-        temp.listing = listingData;
-        res.render('listing', temp);
-      })
-      .catch((errorMessage) => res.send(errorMessage));
-    })
-    .catch((errorMessage) => res.send(errorMessage));
-  });
+    temp.users = UsersData;
+    return commentsQueries.getCommentsById(listingID);
+  })
+  .then((commentData) => {
+    temp.comments = commentData;
+    return listingsQueries.getListing(listingID);
+  })
+  .then((listingData) => {
+    temp.listing = listingData;
+    res.render('listing', temp);
+  })
+  .catch(errorMessage => res.send(errorMessage));
 });
 
 
 
 // GET /listings
 router.get('/', (req, res) => {
-  const userID = req.session.user_id;
-  let temp = {}
 
-  // Three functions here get us query data that was difficult/impossible to join, and passes it to EJS
+  const userID = req.session.user_id;
+
+  let temp = {};
+  temp.user = userID;
+
   usersQueries.getAllUsers()
   .then((UsersData) => {
-    listingsQueries.getAllListings(req.query)
-    .then((listingsData) => {
-      favouritesQueries.getFavouriteListings(userID)
-      .then((favListingsData) => {
-        temp.favourites = favListingsData;
-        temp.users = UsersData;
-        temp.listings = listingsData;
-        temp.user = userID;
-        res.render('listings', temp);
-      })
-      .catch((errorMessage) => res.send(errorMessage));
-    })
+    temp.users = UsersData;
+    return listingsQueries.getAllListings(req.query);
   })
+  .then((listingsData) => {
+    temp.listings = listingsData;
+    return favouritesQueries.getFavouriteListings(userID);
+  })
+  .then((favListingsData) => {
+    temp.favourites = favListingsData;
+    res.render('listings', temp);
+  })
+  .catch(errorMessage => res.send(errorMessage));
 });
 
 
-
+// IMPLEMENT LOGGED IN CHECK
 // POST /listings/:id (post comment)
-
 router.post('/:id', (req, res) => {
 
   // Capture listing, user and comment
-  const listingID = req.params.id;
   const userID = req.session.user_id;
+  const listingID = req.params.id;
   const commentContent = req.body['new-comment'];
 
   commentsQueries.createNewComment(listingID, userID, commentContent)
-  .then(() => {
+  .then((createdComment) => {
     return commentsQueries.getCommentsById(listingID);
-
   })
-  .then((data) => {
-    res.send(data[data.length - 1]);
+  .then((allComments) => {
+    res.send(allComments[allComments.length - 1]);
   })
   .catch((errorMessage) => res.send(errorMessage));
 })
 
 
 
-// POST /listings/:id/favourite
+// POST /listings/:id/favourite (add to favourites)
 router.post('/:id/favourite', (req, res) => {
 
-  // Capture listing id and user id
   const listingID = req.params.id;
   const userID = req.session.user_id;
 
-  // Query to add listing to favourites
   favouritesQueries.addToFavourites(userID, listingID)
   .then(data => {
     res.json(data);
   })
   .catch(errorMessage => res.send(errorMessage));
-
 });
 
 
-// DELETE /listings/:id/favourite
+// DELETE /listings/:id/favourite (remove from favourites)
 router.delete('/:id/favourite', (req, res) => {
 
-  // Capture listing id and user id
   const listingID = req.params.id;
   const userID = req.session.user_id;
 
-  // Query to remove listing from favourites
   favouritesQueries.removeFromFavourites(userID, listingID)
   .then(data => {
     res.json(data);
   })
   .catch(errorMessage => res.send(errorMessage));
-
 });
 
 
 // POST /listings (create new listing)
 router.post('/', (req, res) => {
 
-  // Capture listing attributes from form and owner's id
-  const listingAttributes = {...req.body, owner_id : req.session.user_id};
+  // Capture listing attributes from the form and owner's id
+  const listingAttributes = {
+    ...req.body,
+    owner_id : req.session.user_id
+  };
 
-  // Pass attributes into new listing query
   listingsQueries.createListing(listingAttributes)
   .then((createdListing) => {
-
-    // Placeholder returning newly created listing
     res.redirect('listings/my_listings');
   })
-  .catch((errorMessage) => res.send(errorMessage));
+  .catch(errorMessage => res.send(errorMessage));
 });
 
 
 // PATCH /listings/:id/sold
 router.patch('/:id/sold', (req, res) => {
 
-  // Capture listing id
   const listingID = req.params.id;
 
-  // Query to mark listing as sold
   listingsQueries.markListingSold(listingID)
   .then(soldListing => {
-    console.log('route', soldListing);
     res.json(soldListing);
   })
   .catch(errorMessage => res.send(errorMessage));
@@ -207,10 +192,8 @@ router.patch('/:id/sold', (req, res) => {
 // DELETE /listings/:id (status deleted)
 router.delete('/:id', (req, res) => {
 
-  // Capture listing id parameter
   const listingID = req.params.id;
 
-  // Query to mark listing as deleted
   listingsQueries.deleteListing(listingID)
   .then(data => {
     res.json(data);
